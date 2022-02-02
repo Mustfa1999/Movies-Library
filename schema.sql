9ddc16e891047f58ba4ab9184c56e07be156abfa
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-02-02 14:10:31 +0200

    task 14

--- schema.sql
+++ schema.sql
@@ -1,9 +1,10 @@
 DROP TABLE IF EXISTS movies;
 
-CREATE TABLE IF NOT EXISTS movies (
+CREATE TABLE IF NOT EXISTS movies(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     release_date VARCHAR(255),
     poster_path VARCHAR(10000),
     overview VARCHAR(10000)
 );
+
